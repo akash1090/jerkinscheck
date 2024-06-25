c8839df663636c677c53cf28c939a5979b40a1dc
root <root@ip-172-31-92-71.ec2.internal>
2024-06-25 13:22:15 +0000

    calculate

---
+++ cal/cal.py
@@ -0,0 +1,12 @@
+# Simple calculation to add two numbers
+
+# Define the two numbers
+number1 = 10
+number2 = 20
+
+# Add the numbers
+result = number1 + number2
+
+# Print the result
+print("The sum of", number1, "and", number2, "is:", result)
+
